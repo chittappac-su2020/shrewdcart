fe035be1856dab2d82a0b94ba2335489e43f9cbf
chandrakanth-c <65267759+chandrakanth-c@users.noreply.github.com>
2020-06-18 16:59:09 -0400

    test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-//var request = require('supertest');
+var request = require('supertest');
 var app = require('../app');
 
 it('POST request sanity test', function(done) {
