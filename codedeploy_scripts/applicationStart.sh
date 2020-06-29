cd6ba1c2b3e49d7922370e1bde02dfef80c3d23d
Chandrakanth <chittappa.c@northeastern.edu>
2020-06-29 18:41:26 -0400

    Adding the changes in application start

--- codedeploy_scripts/applicationStart.sh
+++ codedeploy_scripts/applicationStart.sh
@@ -11,6 +11,9 @@ npm start
 echo "Backend started"
 pm2 list
 cd client
+echo "Inside the client directory"
 sudo npm install pm2 --save
+echo "Starting the react app"
 REACT_APP_IP_ADDRESS=$IP_ADDRESS pm2 start node_modules/react-scripts/scripts/start.js --name "client"
 pm2 list
+echo "End"
