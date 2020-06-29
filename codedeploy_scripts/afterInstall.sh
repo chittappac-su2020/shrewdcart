72b63349260b50e9348f774790d538652c582277
Chandrakanth <chittappa.c@northeastern.edu>
2020-06-29 16:16:36 -0400

    Adding the changes in application start

--- codedeploy_scripts/afterInstall.sh
+++ codedeploy_scripts/afterInstall.sh
@@ -1,1 +1,11 @@
 cd /home/ubuntu
+cd /
+sudo chmod 757 userdata.txt
+sudo cp userdata.txt /home/ubuntu
+cd home/ubuntu
+sudo chmod 757 ipadd.txt
+sudo chmod 757 userdata.txt
+cat userdata.txt
+cat ipadd.txt
+cat userdata.txt ipadd.txt > .env
+cat .env
