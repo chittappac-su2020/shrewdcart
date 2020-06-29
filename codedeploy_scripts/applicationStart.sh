f71c529eb61626280b7dbb79269b3f8c8ef07222
Chandrakanth <chittappa.c@northeastern.edu>
2020-06-29 17:31:29 -0400

    Adding the changes in application start

--- codedeploy_scripts/applicationStart.sh
+++ codedeploy_scripts/applicationStart.sh
@@ -8,14 +8,5 @@ sudo chmod 757 userdata.txt
 cat userdata.txt
 cat ipadd.txt
 cat userdata.txt ipadd.txt > .env
+echo "creating the .dot env file"
 cat .env
-npm install --save nodemon
-npm install --save bcrypt
-npm install --save dotenv
-echo "IP address value"
-echo $IP_ADDRESS
-echo "Installing pm2 for the backend"
-sudo npm install pm2 -g -f
-npm start
-echo "Backend started"
-pm2 list
