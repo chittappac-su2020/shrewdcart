a16382a2e1f77815b16b8ccff1f4df601035608f
Chandrakanth <chittappa.c@northeastern.edu>
2020-06-29 21:32:00 -0400

    Adding the changes in application start

--- codedeploy_scripts/applicationStop.sh
+++ codedeploy_scripts/applicationStop.sh
@@ -2,5 +2,5 @@ ls -al
 pm2 stop server
 pm2 stop cloud-ui
 pm2 list
-sudo rm -rf  clientside code_deployment_scripts config controllers models services test  .circleci .gitignore README.md appspec.yml package-lock.json package.json route.js server.js
+sudo rm -rf .circleci api client codedeployment_scripts config controllers front middlewares models test .DS_Store .gitignore README.md app.js appspec.yml package-lock.json package.json server.js
 ls -al
