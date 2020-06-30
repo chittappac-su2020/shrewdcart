1231ba315f61cac0cea557e97aa748ac667b491f
Chandrakanth <chittappa.c@northeastern.edu>
2020-06-29 21:38:01 -0400

    Adding the changes in application start

--- codedeploy_scripts/beforeInstall.sh
+++ codedeploy_scripts/beforeInstall.sh
@@ -1,6 +1,4 @@
+cd /home/ubuntu
 ls -al
-pm2 stop server
-pm2 stop cloud-ui
-pm2 list
 sudo rm -rf .circleci api/routes client codedeployment_scripts config controllers front middlewares models test .DS_Store .gitignore README.md app.js appspec.yml package-lock.json package.json server.js
 ls -al
