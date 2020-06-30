10af02c220b232df0c1b637b1f0c77d10c5439c1
Chandrakanth <chittappa.c@northeastern.edu>
2020-06-30 15:01:28 -0400

    Adding the changes in application start

--- codedeploy_scripts/applicationStart.sh
+++ codedeploy_scripts/applicationStart.sh
@@ -7,10 +7,10 @@ echo "IP address value"
 echo $IP_ADDRESS
 echo "Installing pm2 for the backend"
 sudo npm install pm2 -g -f
-pm2 start server.js -f --name "server"
+pm2 start server.js -f --name "server" 
 echo "Backend started"
 pm2 list
 cd front/
 echo "Inside the client directory"
 npm install
-REACT_APP_IP_ADDRESS=$IP_ADDRESS pm2 start node_modules/react-scripts/scripts/start.js --name "front"
+REACT_APP_IP_ADDRESS=$IP_ADDRESS pm2 start node_modules/react-scripts/scripts/start.js --name "front" -f
