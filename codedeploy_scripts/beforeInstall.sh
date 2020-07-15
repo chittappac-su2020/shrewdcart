006ab7fb1509f2bd8e443e9c9ac4cf871dce58d7
Chandrakanth <chittappa.c@northeastern.edu>
2020-07-15 17:57:30 -0400

    Adding the changes

--- codedeploy_scripts/beforeInstall.sh
+++ codedeploy_scripts/beforeInstall.sh
@@ -5,6 +5,8 @@ pm2 stop front
 sudo rm  /var/lib/apt/lists/lock
 sudo killall lock-frontend
 sudo rm -rf .circleci api client codedeploy_scripts config controllers front middlewares models node_modules userdata.txt test .DS_Store .gitignore README.md app.js appspec.yml package-lock.json package.json server.js
+sudo rm -rf .circleci api client codedeploy_scripts config controllers front middlewares models node_modules userdata.txt test .DS_Store .gitignore README.md app.js appspec.yml package-lock.json package.json server.js
 ls -al
 pm2 stop server
 pm2 stop front
+sudo rm -rf .circleci api client codedeploy_scripts config controllers front middlewares models node_modules userdata.txt test .DS_Store .gitignore README.md app.js appspec.yml package-lock.json package.json server.js
